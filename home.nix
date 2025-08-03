19870968cf5b511dd34bf33271a9361d320b4dc9
Rasmus Buurman <rasmus@rbuurman.de>
2025-08-03 07:41:48 +0200

    XDG config

--- home.nix
+++ home.nix
@@ -15,12 +15,18 @@
     libreoffice
   ];
 
-  home.sessionVariables = {
-    XDG_CACHE_HOME = "$HOME/.cache";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME = "$HOME/.local/share";
-    XDG_STATE_HOME = "$HOME/.local/state";
+  xdg = {
+    enable = true;
 
+    systemDirs.data = [
+      "$HOME/.local/share"
+      "$HOME/.nix-profile/share"
+      "/usr/local/share"
+      "/usr/share"
+    ];
+  };
+
+  home.sessionVariables = {
     CARGO_HOME = "$HOME/.local/share/cargo";
 
     EDITOR = "nvim";
