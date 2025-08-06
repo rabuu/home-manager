5f7e55c9a4e72fcd6dddd87697daf15c59ac1067
Rasmus Buurman <rasmus@rbuurman.de>
2025-08-07 00:16:01 +0200

    Update nvidia setup script

--- debian-setup/nvidia.sh
+++ debian-setup/nvidia.sh
@@ -3,15 +3,14 @@
 set -xe
 
 echo 'TODO: Enable non-free and contrib repositories'
+
 apt update
 
 # apt install nvidia-detect
 
-apt install linux-headers-amd64 nvidia-open-kernel-dkms nvidia-driver firmware-misc-nonfree nvidia-suspend-common
-apt install nvidia-cuda-dev nvidia-cuda-toolkit
+apt install linux-headers-amd64 nvidia-kernel-dkms nvidia-driver firmware-misc-nonfre
+# apt install nvidia-cuda-dev nvidia-cuda-toolkit
 
-systemctl enable nvidia-suspend.service
-systemctl enable nvidia-hibernate.service
-systemctl enable nvidia-resume.service
-
+echo 'options nvidia-current NVreg_PreserveVideoMemoryAllocations=1' >> /etc/modprobe.d/nvidia-options.conf
+echo 'options nvidia-current NVreg_TemporaryFilePath=/var/tmp' >> /etc/modprobe.d/nvidia-options.conf
 echo "options nvidia-drm modeset=1" >> /etc/modprobe.d/nvidia-options.conf
