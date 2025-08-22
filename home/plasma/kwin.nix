bf853b5e11f0d35dcaf424a96e6e85b4708411c2
Rasmus Buurman <rasmus@rbuurman.de>
2025-08-22 19:24:37 +0200

    plasma/kwin: resizeAmount and filters

--- home/plasma/kwin.nix
+++ home/plasma/kwin.nix
@@ -10,7 +10,11 @@
       enable = true;
       settings = {
         borderVisibility = "borderAll";
+        resizeAmount = 80;
         layout.insertionPoint = "right";
+        filter.processes = [
+          "pinentry-qt"
+        ];
       };
     };
   };
