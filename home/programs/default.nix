9da96d297a9794d9663f0e74ca606d02236eb3be
Rasmus Buurman <rasmus@rbuurman.de>
2025-10-13 01:20:10 +0200

    Tidy up programs

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -2,33 +2,20 @@
 {
   home.packages = with pkgs; [
     wl-clipboard
-
     eza
     fd
     ripgrep
-
     fastfetch
     onefetch
     tokei
-
     libreoffice
-    typst
-    tinymist
-    typship
-
     discord
     signal-desktop
     telegram-desktop
     slack
-
     chromium
     tor-browser
-
     vscode
-    jetbrains.idea-ultimate
-
-    testdisk
-
     (config.lib.nixGL.wrap kdePackages.kdenlive)
   ];
 
