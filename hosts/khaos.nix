4e3c2ceb02e7d6b954e859ff171acd03d6d8571d
Rasmus Buurman <rasmus@rbuurman.de>
2025-10-26 23:02:12 +0100

    Remove FreeCAD

--- hosts/khaos.nix
+++ hosts/khaos.nix
@@ -1,8 +1,4 @@
 { config, pkgs, ... }: {
   nixGL.defaultWrapper = "nvidia";
   nixGL.installScripts = [ "nvidia" "mesa" ];
-
-  home.packages = with pkgs; [
-    (config.lib.nixGL.wrap freecad)
-  ];
 }
