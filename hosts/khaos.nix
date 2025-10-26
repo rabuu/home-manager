34739acd922006ee854473dab3bcd7baeafd3a01
Rasmus Buurman <rasmus@rbuurman.de>
2025-10-26 23:13:54 +0100

    Add FreeCAD

--- hosts/khaos.nix
+++ hosts/khaos.nix
@@ -1,4 +1,8 @@
 { config, pkgs, ... }: {
   nixGL.defaultWrapper = "nvidia";
   nixGL.installScripts = [ "nvidia" "mesa" ];
+
+  home.packages = with pkgs; [
+    (config.lib.nixGL.wrap freecad)
+  ];
 }
