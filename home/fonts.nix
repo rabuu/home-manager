c4deceff8e9b5e8b43a5747f5e695ff62826beae
Rasmus Buurman <rasmus@rbuurman.de>
2025-11-28 14:09:40 +0100

    Install fonts for CV

--- home/fonts.nix
+++ home/fonts.nix
@@ -4,5 +4,11 @@
 
   home.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
+
+    # CV (typst template brilliant-cv:2.0.8)
+    roboto
+    source-sans
+    source-sans-pro
+    font-awesome_6
   ];
 }
