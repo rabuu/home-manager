99a41c9c1e51e3debb84cbddcf311ba9590948c6
Rasmus Buurman <rasmus@rbuurman.de>
2025-12-03 23:32:34 +0100

    Replace telescope and harpoon with mini.picker

--- resources/nvim/lua/plugins/mini.lua
+++ resources/nvim/lua/plugins/mini.lua
@@ -8,7 +8,25 @@ return {
 			require("mini.icons").setup()
 			require("mini.notify").setup()
 			require("mini.pairs").setup()
+			require("mini.pick").setup()
 			require("mini.statusline").setup()
 		end,
+		keys = {
+			{
+				"<Leader>.",
+				function() require("mini.pick").builtin.files() end,
+				desc = "Pick files",
+			},
+			{
+				"<Leader>,",
+				function() require("mini.pick").builtin.buffers() end,
+				desc = "Pick buffers",
+			},
+			{
+				"<Leader>/",
+				function() require("mini.pick").builtin.grep_live() end,
+				desc = "Grep live",
+			},
+		},
 	},
 }
--- resources/nvim/lua/plugins/which-key.lua
+++ resources/nvim/lua/plugins/which-key.lua
@@ -5,9 +5,7 @@ return {
 	config = function()
 		require("which-key").add({
 			{ "<leader>c", group = "code" },
-			{ "<leader>t", group = "telescope" },
 			{ "<leader>T", group = "toggle" },
-			{ "<leader>h", group = "harpoon" },
 		})
 	end,
 }
