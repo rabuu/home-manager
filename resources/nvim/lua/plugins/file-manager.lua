e2566669dae38d3b34db9ba74b35e0c25a620dd5
Rasmus Buurman <rasmus@rbuurman.de>
2025-12-04 01:04:55 +0100

    nvim: minor fyler changes

--- resources/nvim/lua/plugins/file-manager.lua
+++ resources/nvim/lua/plugins/file-manager.lua
@@ -6,33 +6,12 @@ return {
 		lazy = false,
 		config = true,
 		keys = {
-			{ "<leader>f", vim.cmd.Fyler, { desc = "Open fyler file manager" } },
+			{ "<leader>f", vim.cmd.Fyler, desc = "open file manager (Fyler)" },
 			{
 				"<leader>F",
 				function() require("fyler").open({ kind = "split_left_most" }) end,
-				{ desc = "Open fyler file manager (split)" }
+				desc = "open file manager in split (Fyler)"
 			},
 		},
 	}
-	-- {
-	-- 	"stevearc/oil.nvim",
-	-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- 	opts = {
-	-- 		default_file_explorer = true,
-	-- 		skip_confirm_for_simple_edits = true,
-	-- 		view_options = {
-	-- 			show_hidden = true,
-	-- 		},
-	-- 		keymaps = {
-	-- 			["q"] = "actions.close",
-	-- 			["^"] = "actions.parent",
-	-- 		},
-	-- 	},
-	-- 	keys = { { "<leader>f", vim.cmd.Oil, desc = "oil file manager" } },
-	-- 	cmd = "Oil",
-	-- 	config = function(_, opts)
-	-- 		require("oil").setup(opts)
-	-- 		vim.api.nvim_set_hl(0, "OilDirIcon", { link = "NvimTreeFolderIcon" })
-	-- 	end,
-	-- },
 }
