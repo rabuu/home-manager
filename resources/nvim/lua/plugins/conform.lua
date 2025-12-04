457bc1b8782255289aab42310e9aebcde5ccd607
Rasmus Buurman <rasmus@rbuurman.de>
2025-12-04 01:40:22 +0100

    nvim: dont format by default

--- resources/nvim/lua/plugins/conform.lua
+++ resources/nvim/lua/plugins/conform.lua
@@ -5,13 +5,11 @@ return {
 		opts = {
 			formatters_by_ft = {
 				rust = { "rustfmt" },
+				lsp_format = "fallback",
 			},
 			format_on_save = {
 				timeout_ms = 500,
 			},
-			default_format_opts = {
-				lsp_format = "fallback",
-			}
 		},
 		keys = {
 			{ "<leader>cf", function() require("conform").format() end, desc = "format code (Conform)" },
