c14b7024fca2857668c8e0e14a5662f7d6e5917a
Rasmus Buurman <rasmus@rbuurman.de>
2025-12-04 01:59:42 +0100

    nvim: colorscheme minor changes

--- resources/nvim/lua/plugins/colorscheme.lua
+++ resources/nvim/lua/plugins/colorscheme.lua
@@ -6,8 +6,7 @@ return {
 		config = function()
 			vim.g.gruvbox_material_transparent_background = 1
 			vim.g.gruvbox_material_foreground = "original"
-			vim.g.gruvbox_material_enable_italic = true
-			vim.g.gruvbox_material_enable_bold = true
+			vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
 			vim.cmd.colorscheme("gruvbox-material")
 		end,
 	},
