5e53915679874872ffdaa427a9e715c5da3fd4e7
Rasmus Buurman <rasmus@rbuurman.de>
2025-12-04 01:16:41 +0100

    nvim: Enable cursorline

--- resources/nvim/lua/settings.lua
+++ resources/nvim/lua/settings.lua
@@ -8,6 +8,7 @@ vim.opt.cindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.number = true
+vim.opt.cursorline = true
 vim.opt.termguicolors = true
 vim.opt.statuscolumn = "%l%s"
 vim.opt.signcolumn = "yes:1"
