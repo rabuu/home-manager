0c364d5ca931c13fbee4a75c6cdb86af846e86b6
Rasmus Buurman <rasmus@rbuurman.de>
2026-01-09 20:44:11 +0100

    Update git settings names

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -3,24 +3,27 @@
   programs.git = {
     enable = true;
 
-    userName = "Rasmus Buurman";
-    userEmail = "rasmus@rbuurman.de";
+    settings = {
+      user = {
+        name = "Rasmus Buurman";
+        email = "rasmus@rbuurman.de";
+      };
 
-    aliases = {
-      co = "checkout";
-      st = "status";
-      br = "branch";
-      cm = "commit";
-      df = "diff";
-      dfc = "diff --cached";
-      sw = "switch";
-      rs = "restore";
-    };
+      alias = {
+        co = "checkout";
+        st = "status";
+        br = "branch";
+        cm = "commit";
+        df = "diff";
+        dfc = "diff --cached";
+        sw = "switch";
+        rs = "restore";
+      };
 
-    extraConfig = {
       init = {
         defaultBranch = "main";
       };
+
       push = {
         autoSetupRemote = true;
       };
