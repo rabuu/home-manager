077be5b6b85e7ba8248545ae9a1b86e0370d1cbd
Rasmus Buurman <rasmus@rbuurman.de>
2026-04-05 18:32:51 +0200

    nvim: add lsp label to which-key gr group

--- other/nvim/plugin/which-key.lua
+++ other/nvim/plugin/which-key.lua
@@ -5,6 +5,7 @@ local wk = require("which-key")
 wk.setup()
 
 wk.add({
+	{ "<leader>gr", group = "lsp" },
 	{ "<leader>c", group = "code" },
 	{ "<leader>T", group = "toggle" },
 	{ "<leader>P", group = "pack" },
