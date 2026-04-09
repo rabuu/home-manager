cd6450717f29e1bb3b334f707a1073dbda2b266d
Rasmus Buurman <rasmus@rbuurman.de>
2026-04-09 13:39:30 +0200

    pass: set PASSWORD_STORE_DIR env variable

--- home/programs/pass.nix
+++ home/programs/pass.nix
@@ -6,6 +6,10 @@
     settings = { };
   };
 
+  home.sessionVariables = {
+    PASSWORD_STORE_DIR = "$HOME/.local/share/password-store";
+  };
+
   # HACK(2025-08-03): Fish completion not working OOTB for pass
   xdg.configFile."fish/completions/pass.fish" = {
     source = "${pkgs.pass}/share/fish/vendor_completions.d/pass.fish";
