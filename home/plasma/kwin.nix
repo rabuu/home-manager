9ca0e9aa5da9172fe4d9bebd3e4dfa581e11dff0
Rasmus Buurman <rasmus@rbuurman.de>
2026-09-14 21:57:48 +0200

    plasma: remove polonium

--- home/plasma/kwin.nix
+++ home/plasma/kwin.nix
@@ -5,17 +5,5 @@
       number = 6;
       rows = 2;
     };
-
-    scripts.polonium = {
-      enable = true;
-      settings = {
-        borderVisibility = "noBorderAll";
-        resizeAmount = 80;
-        layout.insertionPoint = "right";
-        filter.processes = [
-          "pinentry-qt"
-        ];
-      };
-    };
   };
 }
